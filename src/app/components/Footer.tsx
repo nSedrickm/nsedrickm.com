4edcf735e7f4e032cda396bfc5d3accb102af967
nsedrickm <nsedrick101@gmail.com>
2023-05-25 21:38:33 +0100

    update: add scrolling carousel for testimonials

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,14 +1,20 @@
 import { Facebook, Github, Linkedin, Twitter } from "lucide-react";
 import { Logo } from "./Logo";
 import Image from "next/image";
+import Link from "next/link";
 
 export const Footer = () => {
   return (
     <footer className="text-center bg-gradient-to-b from-gray-500/25 via-gray-800/25 to-black backdrop-blur">
       <div className="flex flex-col flex-wrap items-center gap-8 p-4 md:p-8">
         <Logo />
-        <p className="">For any queries, please feel free to contact me</p>
-        <div className="flex col-span-1 gap-8 jitems-center">
+        <p className="">
+          For any queries, please feel free to{" "}
+          <Link href="/contact" className="underline underline-offset-1">
+            contact me
+          </Link>
+        </p>
+        <div className="flex items-center col-span-1 gap-0 md:gap-2">
           <a
             href="https://www.linkedin.com/in/ngwa-sedrick-meh/"
             target="_blank"
@@ -40,7 +46,7 @@ export const Footer = () => {
         </div>
       </div>
 
-      <p className="self-stretch p-4 text-white bg-blue-700">
+      <p className="self-stretch p-4 text-base text-white bg-blue-700">
         © 2023 nsedrickm all rights reserved
       </p>
     </footer>
--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -1,6 +1,9 @@
+"use client";
+import { useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import clsx from "clsx";
+import { ChevronRight, ChevronLeft } from "lucide-react";
 import img1 from "src/app/images/testimonials/img-1.png";
 import img2 from "src/app/images/testimonials/img-2.png";
 import img3 from "src/app/images/testimonials/img-3.png";
@@ -11,6 +14,17 @@ import img6 from "src/app/images/testimonials/img-6.png";
 const reviews = [img1, img2, img3, img4, img5, img6];
 
 export const Testimonials = () => {
+  const conRef = useRef<HTMLDivElement>(null);
+
+  function scrollContainer(direction: string) {
+    if (conRef.current) {
+      conRef.current.scrollBy({
+        top: 0,
+        left: direction === "back" ? -480 : 480,
+        behavior: "smooth",
+      });
+    }
+  }
   return (
     <section
       id="services"
@@ -24,29 +38,50 @@ export const Testimonials = () => {
         are some of the reviews I have received from my clients on Upwork.
       </p>
 
-      <div className="grid gap-8 my-10 text-left md:grid-cols-2 lg:grid-cols-3">
-        {reviews.map((item, index) => (
-          <div
-            key={index}
-            className="relative w-full h-56 rounded-md md:h-72 group shadow-3xl"
-          >
-            <Image
-              src={item}
-              fill
-              alt="upwork review"
-              className="absolute inset-0 object-contain p-0 m-0"
-            />
-
-            <div className="absolute inset-0 items-center justify-center hidden group-hover:flex backdrop-blur-sm bg-black/40">
-              <a
-                href="https://www.upwork.com/freelancers/~010a12bb6327f60ed1"
-                className="p-4 font-bold text-black no-underline transition duration-300 ease-in-out delay-150 bg-white rounded-md md:py-4 md:px-8 hover:-translate-y-1 hover:scale-110"
-              >
-                View my profile
-              </a>
+      <div className="relative flex items-center justify-center gap-2 my-10 mb-10">
+        <div className="absolute bottom-0 z-10 grid p-2 translate-y-1 rounded-full md:p-8 md:static left-20 place-items-center backdrop-blur">
+          <ChevronLeft
+            role="button"
+            className="w-10 h-10 p-2 text-black bg-white border rounded-full md:w-12 md:h-12 md:p-3"
+            onClick={() => scrollContainer("back")}
+          />
+        </div>
+        <div
+          className="flex gap-6 overflow-x-auto snap-mandatory snap-x no-scrollbar"
+          ref={conRef}
+        >
+          {reviews.map((item, index) => (
+            <div
+              key={index}
+              className="relative flex-none h-52 rounded-md md:h-72 group shadow-3xl w-[20rem] md:w-[30rem] shadow-3xl snap-center"
+            >
+              <Image
+                fill
+                key={index}
+                src={item}
+                alt="upwork review"
+                className="object-contain p-0 m-0"
+              />
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
+
+        <div className="absolute bottom-0 z-10 grid p-2 translate-y-1 rounded-full md:p-8 md:static right-20 place-items-center backdrop-blur">
+          <ChevronRight
+            role="button"
+            className="w-10 h-10 p-2 text-black bg-white border rounded-full md:w-12 md:h-12 md:p-3"
+            onClick={() => scrollContainer("forward")}
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-center my-10">
+        <Link
+          href="/contact?service=upwork" // href="https://www.upwork.com/freelancers/~010a12bb6327f60ed1"
+          className="p-4 font-bold text-black no-underline transition duration-300 ease-in-out delay-150 bg-white rounded-md hover:bg-blue-700 hover:text-white md:py-4 md:px-8 hover:-translate-y-1 hover:scale-110"
+        >
+          {"Let's Collaborate Today!"}
+        </Link>
       </div>
     </section>
   );
