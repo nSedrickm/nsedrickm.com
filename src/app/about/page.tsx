5e5fa9539c3dee6f576c3dd4a31285456103d82e
nsedrickm <nsedrick101@gmail.com>
2023-05-26 23:16:06 +0100

    update: switchout animations

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,22 +1,34 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import headerBg from "images/header-background.png";
 import { Technologies, Awards } from "./components";
 import { PageAnimationWrapper } from "components/PageAnimationWrapper";
+import { motion } from "framer-motion";
 
 const About = () => {
+  const variants = {
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, y: -20 },
+  };
   return (
     <PageAnimationWrapper layoutId="about">
       <header className="flex flex-col space-y-8 lg:flex-row col-span-full">
         <div className="flex flex-col self-center flex-1 gap-6 p-6 sm:p-16">
-          <h1 className="text-3xl font-extrabold tracking-wide md:text-4xl">
+          <motion.h1
+            variants={variants}
+            className="text-3xl font-extrabold tracking-wide md:text-4xl"
+          >
             About Me
-          </h1>
-          <h2 className="text-4xl font-light leading-tight md:text-5xl">
+          </motion.h1>
+          <motion.h2
+            variants={variants}
+            className="text-4xl font-light leading-tight md:text-5xl"
+          >
             Ngwa Sedrick Meh
-          </h2>
+          </motion.h2>
 
-          <p className="">
+          <motion.p variants={variants}>
             Ngwa Sedrick Meh is a freelance software developer with a passion
             for developing web applications. He is particularly known for his
             expertise in building ReactJS applications and is a Top Rated
@@ -24,23 +36,23 @@ const About = () => {
             achieve their goals through the innovative and effective apps he
             builds. Caring deeply about his clients success he is dedicated to
             delivering top-quality results that help them thrive.
-          </p>
+          </motion.p>
 
-          <p>
+          <motion.p variants={variants}>
             He holds a Bachelor of Technology in Computer Networking and Systems
             Maintenance and was a Google Summer of Code participant in 2022 with
             RedHat, now IBM Ceph Dashboard team. He is also a two-time IBM
             Master the Mainframe Part 3 finisher.
-          </p>
+          </motion.p>
 
-          <p>
+          <motion.p variants={variants}>
             When not working, Ngwa Sedrick Meh, who also goes by the pseudonym
             nsedrickm, spends his time exploring cars, contributing to open
             source projects, and spending time with his loved ones. With his
             expertise, dedication, and commitment to continuous learning and
             growth, he actively participates in his local developer community
             and joins conversations on social hubs.
-          </p>
+          </motion.p>
         </div>
         <div className="relative flex-1">
           <Image
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,28 +4,41 @@ import Image from "next/image";
 import headerBg from "images/header-background.png";
 import { ChevronRight } from "lucide-react";
 import { Services, Testimonials, PageAnimationWrapper } from "components";
-import { motion, useIsPresent } from "framer-motion";
+import { motion } from "framer-motion";
 
 const Home = () => {
+  const variants = {
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
+    hidden: { opacity: 0, y: -20 },
+  };
+
   return (
     <PageAnimationWrapper layoutId="home">
       <header className="flex flex-col space-y-8 lg:flex-row col-span-full">
         <div className="flex flex-col self-center flex-1 gap-8 p-6 mt-20 sm:p-16">
-          <h1 className="text-5xl font-extrabold tracking-wide md:text-6xl">
+          <motion.h1
+            variants={variants}
+            className="text-5xl font-extrabold tracking-wide md:text-6xl"
+          >
             Welcome!
-          </h1>
-          <p className="text-2xl font-light leading-normal md:text-5xl">
+          </motion.h1>
+          <motion.p
+            variants={variants}
+            className="text-2xl font-light leading-normal md:text-5xl"
+          >
             My name is <strong className="font-semibold">Sedrick</strong> and I
             build and maintain web applications
-          </p>
+          </motion.p>
 
-          <Link
-            href="#services"
-            className="flex items-center self-start justify-center gap-2 py-4 text-xl text-white transition duration-300 ease-in-out delay-150 border-b border-white md:text-2xl group"
-          >
-            <span>Learn more</span>
-            <ChevronRight />
-          </Link>
+          <motion.span variants={variants} className="self-start">
+            <Link
+              href="#services"
+              className="flex items-center justify-center gap-2 py-4 text-xl text-white transition duration-300 ease-in-out delay-150 border-b border-white md:text-2xl group"
+            >
+              <span>Learn more</span>
+              <ChevronRight />
+            </Link>
+          </motion.span>
         </div>
         <div className="relative flex-1">
           <Image
